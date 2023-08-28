7b74f2af128df2b634a95285b6499a57e5a698b1
Palm78070 <rath.palm@gmail.com>
2023-08-28 20:33:17 +0700

    before insert env to mariadb

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -10,38 +10,75 @@
 #
 #mysql -u root < db.sql
 #service mysql restart
-#tail -f /dev/null
 
-#service mysql start
-#chown mysql:mysql /var/run/mysqld/
-#chmod -R 755 /var/run/mysqld/
+
+mkdir -p /var/run/mysqld
+chown mysql:mysql /var/run/mysqld/
+chmod -R 755 /var/run/mysqld/
 
 /etc/init.d/mysql start
 
 sleep 5
 # Check if the database already exists -> if no database throw message error(fd 2) to /dev/null
-if mysql -uroot -pP@lm78070 -e "USE WordpressDB;" 2>/dev/null; then
-    echo "Database already exists"
+#if mysql -uroot -pP@lm78070 -e "USE WordpressDB;" 2>/dev/null; then
+if [ -d "/var/lib/mysql/WordpressDB" ]; then
+	echo "Database already exists"
 else
-    echo "CREATE DATABASE IF NOT EXISTS WordpressDB ;" > db.sql
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'P@lm78070' ;" >> db.sql
-    echo "CREATE USER IF NOT EXISTS 'Palm'@'%' IDENTIFIED BY '1234' ;" >> db.sql
-    echo "GRANT ALL PRIVILEGES ON WordpressDB.* TO 'Palm'@'%' ;" >> db.sql
-    echo "FLUSH PRIVILEGES;" >> db.sql
+	echo "CREATE DATABASE IF NOT EXISTS WordpressDB ;" > db.sql
+	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'P@lm78070' ;" >> db.sql
+	echo "CREATE USER IF NOT EXISTS 'Palm'@'%' IDENTIFIED BY '1234' ;" >> db.sql
+	echo "GRANT ALL PRIVILEGES ON WordpressDB.* TO 'Palm'@'%' ;" >> db.sql
+	echo "FLUSH PRIVILEGES;" >> db.sql
 
-    mysql -uroot -pP@lm78070 < db.sql
+	mysql -uroot -pP@lm78070 < db.sql
+
+	#Stop mysql server
+	mysqladmin -uroot -pP@lm78070 shutdown
+
+	#Wait for mysql server to stop
+	while ps aux | grep -v grep | grep mysqld; do
+		sleep 1
+	done
+
+	# --user=mysql -> run mysqld with user mysql which have limited system privileges for security reason
+	# --console -> send mysql error log/other message to terminal instead of log file
+	# --skip-name-resolve -> prevent mysql server from DNS lookups when clients connect(long delay and slow performance)
+	# --skip-networking=0 -> 0 enables network connections to the MySQL server(if 1 you connect only through unix socket)
+	mysqld_safe --user=mysql --console --skip-name-resolve --skip-networking=0
 fi
 
-#Stop mysql server
-mysqladmin -uroot -pP@lm78070 shutdown
-
-#Wait for mysql server to stop
-while ps aux | grep -v grep | grep mysqld; do
-    sleep 1
-done
-
-# --user=mysql -> run mysqld with user mysql which have limited system privileges for security reason
-# --console -> send mysql error log/other message to terminal instead of log file
-# --skip-name-resolve -> prevent mysql server from DNS lookups when clients connect(long delay and slow performance)
-# --skip-networking=0 -> 0 enables network connections to the MySQL server(if 1 you connect only through unix socket)
-mysqld --user=mysql --console --skip-name-resolve --skip-networking=0
+######################################################################
+#mkdir -p /var/run/mysqld
+#chown mysql:mysql /var/run/mysqld/
+#chmod -R 755 /var/run/mysqld/
+#
+#/etc/init.d/mysql start
+#
+#sleep 5
+## Check if the database already exists -> if no database throw message error(fd 2) to /dev/null
+##if mysql -uroot -pP@lm78070 -e "USE WordpressDB;" 2>/dev/null; then
+#if [ -d "/var/lib/mysql/WordpressDB" ]; then
+#	echo "Database already exists"
+#else
+#	echo "CREATE DATABASE IF NOT EXISTS WordpressDB ;" > db.sql
+#	echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'P@lm78070' ;" >> db.sql
+#	echo "CREATE USER IF NOT EXISTS 'Palm'@'%' IDENTIFIED BY '1234' ;" >> db.sql
+#	echo "GRANT ALL PRIVILEGES ON WordpressDB.* TO 'Palm'@'%' ;" >> db.sql
+#	echo "FLUSH PRIVILEGES;" >> db.sql
+#
+#	mysql -uroot -pP@lm78070 < db.sql
+#
+#	#Stop mysql server
+#	mysqladmin -uroot -pP@lm78070 shutdown
+#
+#	#Wait for mysql server to stop
+#	while ps aux | grep -v grep | grep mysqld; do
+#		sleep 1
+#	done
+#
+#	# --user=mysql -> run mysqld with user mysql which have limited system privileges for security reason
+#	# --console -> send mysql error log/other message to terminal instead of log file
+#	# --skip-name-resolve -> prevent mysql server from DNS lookups when clients connect(long delay and slow performance)
+#	# --skip-networking=0 -> 0 enables network connections to the MySQL server(if 1 you connect only through unix socket)
+#	mysqld_safe --user=mysql --console --skip-name-resolve --skip-networking=0
+#fi
